1979616417daa82fc09afe671abc24e6ca53b0ea
Mei <18771886893@163.com>
2017-11-22 10:22:16 +0800

    add:code

---
+++ TextCocoapods/TextCocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextCocoapods
+//
+//  Created by FengkunMei on 2017/11/22.
+//  Copyright © 2017年 mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
